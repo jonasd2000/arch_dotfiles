ece7e30084a8d64312616f01b685f92e7a487a78
Jonas Dupré <jonasdupre@yahoo.de>
2024-09-20 19:48:30 +0200

    reverted to config_picker.py

---
+++ config_picker.py
@@ -0,0 +1,13 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument()
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+if __name__ == "__main__":
+    main()
