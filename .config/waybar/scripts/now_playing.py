26ffe0e57eb3e029580ef7d3462b9af379f045e8
Jonas Dupré <jonasdupre@yahoo.de>
2024-09-29 15:59:26 +0200

    waybar moved now_playing script to python including player priority

---
+++ .config/waybar/scripts/now_playing.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+import subprocess
+import sys
+import re
+
+player_priority = ["spotify", "firefox"]
+
+get_players = subprocess.run(["playerctl", "-l"], capture_output=True)
+players = [
+    {"name": player} for player in
+    get_players.stdout.decode().split("\n")[:-1]
+]
+
+for player in players:
+    get_status = subprocess.run(
+            f"playerctl status --player {player["name"]}".split(),
+            capture_output=True
+    )
+    player["status"] = get_status.stdout.decode().strip()
+    player["priority"] = [i for i, item in enumerate(player_priority) if re.search(item, player["name"])] or [100]
+
+active_players = list(filter(lambda pl: pl["status"] == "Playing", players))
+
+if not active_players:
+    print("")
+    sys.exit(1)
+
+
+active_players_prio = sorted(active_players, key=lambda pl: pl["priority"][0])
+subprocess.run(f"playerctl metadata --player {active_players_prio[0]["name"]} --format {{{{artist}}}}-{{{{title}}}}".split())
