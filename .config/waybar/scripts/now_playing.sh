8f1ef13b2c9dcdb04cb300eec149441d006b21b8
Jonas Dupré <jonasdupre@yahoo.de>
2024-09-29 13:50:28 +0200

    waybar now_playing script extended to more statuses

--- .config/waybar/scripts/now_playing.sh
+++ .config/waybar/scripts/now_playing.sh
@@ -1,5 +1,17 @@
-if [[ "$(playerctl status)" == Paused ]]; then
+#!/usr/bin/bash
+
+status_paused="Paused"
+status_no_players="No players found"
+status_playing="Playing"
+
+current_status="$(playerctl status 2>&1)"
+
+if [[ "$current_status" == "$status_paused" ]]; then
     echo ""
-    exit 1;
+    exit 1
+elif [[ "$current_status" == "$status_no_players" ]]; then
+    echo ""
+    exit 1
+elif [[ "$current_status" == "$status_playing" ]]; then
+    echo "$(playerctl metadata artist) - $(playerctl metadata title)"
 fi
-echo "$(playerctl metadata artist) - $(playerctl metadata title)"
