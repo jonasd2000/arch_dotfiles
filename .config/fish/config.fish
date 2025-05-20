8bf005804c5b74a7d53e9f331766e7575b9c8afc
Jonas Dupré <jonasdupre@yahoo.de>
2025-05-20 17:53:40 +0200

    nvim as default editor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
+set -gx EDITOR nvim
+
 abbr --add py python3
 abbr --add spt spotify_player
+abbr --add nv nvim
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
