0ceef3c49cdf321e1c38d8fe1fbf52a4e05411ce
Jonas Dupré <jonasdupre@yahoo.de>
2025-05-20 17:40:59 +0200

    nvim system clipboard

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
+local options = {
+  clipboard = "unnamedplus", --system clipboard
+}
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
