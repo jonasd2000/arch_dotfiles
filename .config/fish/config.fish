778fc24ccf114867c78231bcc1b0b05b6b0e8411
Jonas Dupré <jonasdupre@yahoo.de>
2025-05-22 20:14:06 +0200

    small weather widget

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ abbr --add py python3
 abbr --add spt spotify_player
 abbr --add nv nvim
 abbr --add inv fzf --bind '"enter:become(nvim {})"' --preview '"cat {}"'
+abbr --add bt bluetuith
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
