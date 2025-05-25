036f25438d5963e9c34d324af0344a96ca20247b
Jonas Dupré <jonasdupre@yahoo.de>
2025-05-25 19:03:04 +0200

    nvim tab 4 spaces

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,9 +2,8 @@ require "nvchad.options"
 
 -- add yours here!
 
-local options = {
-  clipboard = "unnamedplus", --system clipboard
-}
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+o.clipboard = "unnamedplus"
+o.shiftwidth = 4
